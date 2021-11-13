5208305e802d32945e907ef2de0fa8eaf20eb205
Zamal Hossain <thezamalian@gmail.com>
2021-11-13 11:12:03 +0600

    database is connected successfully

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwjoa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log('DB connected successfully');
+    }
+    finally {
+        // await client.close(); 
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-    res.send('Running Doctors Portals server!!!!!!!');
+    res.send('Running Brainy Drone server!!!!!!!');
 })
 
 app.listen(port, () => {
